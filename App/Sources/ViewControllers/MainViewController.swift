22b1f099a235774a7a65ec12ae94177ae5a29ebe
Rami Ounifi <rami.ounifi@esprit.tn>
2025-03-16 01:19:53 +0100

    add images
    images assets

--- App/Sources/ViewControllers/MainViewController.swift
+++ App/Sources/ViewControllers/MainViewController.swift
@@ -3,12 +3,31 @@ import UIKit
 class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-
+        
+        setupBackground()
+        setupLabel()
+    }
+    
+    private func setupBackground() {
+        let backgroundImageView = UIImageView(image: UIImage(named: "Calque 0"))
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(backgroundImageView)
+        
+        NSLayoutConstraint.activate([
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    private func setupLabel() {
         let label = UILabel()
         label.text = "Welcome to Douleb App!"
         label.textAlignment = .center
         label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .black // Ensure visibility over the background
 
         label.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(label)
@@ -19,4 +38,3 @@ class MainViewController: UIViewController {
         ])
     }
 }
-
